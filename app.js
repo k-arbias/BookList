14932c37bb2bd66a1af6197c34ef5b165d553372
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-11-17 18:14:08 +0100

    Functions to add books to the list

--- app.js
+++ app.js
@@ -7,18 +7,40 @@ class Book{
 }
 
 class UI{
-    addBookToList(){
+    addBookToList(book){
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    showAlert(message, className){
 
     }
 
-    showAlert(){
-
-    }
-
-    deleteBook(){
+    deleteBook(target){
 
     }
     clearFields(){
-        
+
     }
 }
+document.getElementById('book-form').addEventListener('submit', function(e){
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value;
+
+    const book = new Book(title, author, isbn);
+    
+    const ui = new UI();
+
+    ui.addBookToList(book);
+
+    e.preventDefault();
+});
